ac3eb7253460192193291ccbee6ad804b2a02a5b
Arnaud Locquet <alocquet@gmail.com>
2017-06-22 13:11:42 +0200

    implement test

--- clients/javascript/express/test/200-process-order.js
+++ clients/javascript/express/test/200-process-order.js
@@ -9,4 +9,11 @@ describe('process.order(payload, callback)', function () {
       done();
     })
   })
+  it('calls back with a test', function (done) {
+    process.order({ "prices": [4.1, 8.03, 86.83, 65.62, 44.82], "quantities": [10, 3, 5, 4, 5], "country": "AT", "reduction": "STANDARD" }, function (err, result) {
+      if (err) return done(err);
+      expect(result).to.deep.equal({total: 1166.62});
+      done();
+    })
+  })
 })
